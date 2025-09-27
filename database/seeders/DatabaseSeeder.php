0129f492e5f1b157b08a207029880938f6060983
Mariem Habouria <mariemhabouria00@gmail.com>
2025-09-27 19:00:32 +0100

    updates 2.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,17 +9,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Crée quelques utilisateurs basiques
-        \App\Models\User::factory(10)->create();
-
-        // ⚡ Crée un admin fixe
-        \App\Models\User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@tunivert.tn',
-            'password' => bcrypt('admin123'), // mot de passe pour se connecter
-            'role' => 'admin',
-            'is_association' => false,
-            'matricule_association' => null,
-        ]);
+        \App\Models\User::factory(20)->create();
 
         // Appelle le seeder de challenges
         $this->call(ChallengeSeeder::class);
